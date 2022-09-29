b200e51f38aabf94aac179dc6683d97f9905d38d
kabosu-YN <93434066+kabosu-YN@users.noreply.github.com>
2022-09-29 18:30:41 +0900

    parallax ソース追加

---
+++ parallax/luxy/js/script.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function(){
+    luxy.init()
+
+    let elmScrollTrigger = document.getElementsByClassName('scroll_trigger')
+
+    let scrollTopPos = window.pageYOffset;
+        let elmMoreBtnPosArray = new Array();
+    
+    // morebtnの上座標取得
+    for(i=0; i < elmScrollTrigger.length; i++){
+        let elmMoreBtnRect = elmScrollTrigger[i].getBoundingClientRect();
+        elmMoreBtnPosArray[i] = elmMoreBtnRect.top;
+    }
+
+    window.addEventListener('scroll',function(){
+
+        for(i=0; i < elmScrollTrigger.length; i++){
+
+            scrollTopPos = window.pageYOffset + window.innerHeight - 100;
+            elmMoreBtnPos = elmMoreBtnPosArray[i];
+
+            if(scrollTopPos > elmMoreBtnPos){
+                elmScrollTrigger[i].classList.add('active')
+            }
+        }
+    });
+})
